58ef017402462c86a8baf94f5753981bbad26f74
Merlin <i.lukyanov@weplay.tv>
2020-02-19 09:13:49 +0200

    feature(): [] remove-initial-props
      Removed initial props

--- packages/suto-ssr/src/_pages/_app/index.js
+++ packages/suto-ssr/src/_pages/_app/index.js
@@ -8,7 +8,6 @@ import App from 'next/app'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import withRedux from 'next-redux-wrapper'
 
-import { CANT_STRINGIFY, safeStringify } from '../../_common/_helpers/safeStringify'
 import ModalsProvider from '../../_common/ModalsProvider/ModalsProvider'
 import LocaleProvider from '../../_common/LocaleProvider/LocaleProvider'
 import '../../_common/styles/base.scss'
@@ -23,35 +22,6 @@ const modalInitialValues = {
 }
 // ========== executed once on the client, maybe there is some better place for config
 class MyApp extends App {
-  static async getInitialProps({
-    Component, ctx, router, store,
-  }) {
-    // TODO: think about normal solution
-    const hasRuLang = ctx.asPath.split('/')[1] === 'ru'
-    const initialLocale = hasRuLang ? 'ru' : (router.query.language || 'en')
-
-    const pagePropsPromise = Component.getInitialProps ? Component.getInitialProps({
-      ctx,
-      router,
-      store,
-      initialLocale,
-    }) : {}
-    const [
-      pageProps,
-    ] = await Promise.all([
-      pagePropsPromise,
-    ])
-    const stringifiedProps = safeStringify(pageProps)
-    if (stringifiedProps.status === CANT_STRINGIFY) {
-      console.warn('CIRCULAR DEPENDENCY IN PAGE PROPS', stringifiedProps.parsed)
-    }
-    return {
-      pageProps: stringifiedProps.parsed,
-      initialLocale,
-      canonicalUrl: router.asPath.split('?')[0],
-    }
-  }
-
   render() {
     const {
       Component: Page,
