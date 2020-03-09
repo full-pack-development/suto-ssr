91fcc60a2c644923d6e0ae5fe0a923aef53c15f8
Oleg Kotsubko <olegkotsubko@gmail.com>
2020-03-09 23:43:34 +0200

    feature(): [] add-localize-to-text-blocks
      added localize to text blocks

--- packages/suto-ssr/src/_pages/options/index.js
+++ packages/suto-ssr/src/_pages/options/index.js
@@ -7,41 +7,45 @@ import Brands from '_common/components/Brands'
 import SectionSubscribe from '_common/components/SectionSubscribe'
 /** Helpers */
 import { concatZeroToIndex } from '_common/_helpers/concatZeroToIndex'
+import { useTranslation } from '_common/LocaleProvider/useTranslation'
 
 import { textSections } from '../textSections'
 import { dataCapsules } from '../dataCapsules'
 
 const OptionsPage = ({
-}) => (
-  <>
-    {dataCapsules.map((capsule, index) => (
-      <Capsule
-        key={capsule.id}
-        accent={capsule.color}
-        images={capsule.images}
-        titleFirst={capsule.titleFirst}
-        titleSecond={capsule.titleSecond}
-        subTitle={capsule.subTitle}
-        button={capsule.button}
-        index={concatZeroToIndex(index)}
-      />
-    ))}
-    <Wrapper>
-      <TextSection
-        title={textSections.textSectionFirst.title}
-        text={textSections.textSectionFirst.text}
-      />
-    </Wrapper>
-    <SectionSeven data={textSections.sectionSeven} />
-    <Brands data={textSections.brands} />
-    <SectionSubscribe data={textSections.subscribe} />
-    <Wrapper>
-      <TextSection
-        italic
-        title={textSections.textSectionSix.title}
-      />
-    </Wrapper>
-  </>
-)
+}) => {
+  const t = useTranslation()
+  return (
+    <>
+      {dataCapsules.map((capsule, index) => (
+        <Capsule
+          key={capsule.id}
+          accent={capsule.color}
+          images={capsule.images}
+          titleFirst={capsule.titleFirst}
+          titleSecond={capsule.titleSecond}
+          subTitle={capsule.subTitle}
+          button={capsule.button}
+          index={concatZeroToIndex(index)}
+        />
+      ))}
+      <Wrapper>
+        <TextSection
+          title={t('textSectionFirst.title')}
+          text={t('textSectionFirst.text')}
+        />
+      </Wrapper>
+      <SectionSeven image={textSections.sectionSeven.image} />
+      <Brands data={textSections.brands} />
+      <SectionSubscribe />
+      <Wrapper>
+        <TextSection
+          italic
+          title={t('textSectionSix.title')}
+        />
+      </Wrapper>
+    </>
+  )
+}
 
 export default OptionsPage
